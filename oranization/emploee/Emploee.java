444b5b52abe7c7598ea97921e7702fac15cd3b0a
Aidos <aidoz@mail.ru>
2020-03-02 21:38:43 +0600

    HW-3 sorting collection

---
+++ oranization/emploee/Emploee.java
@@ -0,0 +1,37 @@
+package com.epam.oranization.emploee;
+
+public abstract class Emploee {
+    private String name;
+    private int id;
+    private int salary;
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+}
